e4cbc625b164811b73717a77d478ee63e271dc39
IvanIniavkin <inyvkin94@gmail.com>
2023-03-25 16:32:27 +0400

    V1.0

---
+++ script.ру.py
@@ -0,0 +1,9 @@
+deposit = []
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = float(input("Введите сумму, которую планируете положить:"))
+for key in per_cent:
+    deposit.append(per_cent[key] * money/100)
+print(deposit)
+max_deposit = max(deposit)
+print ("Максимальная сумма, которую вы можете заработать — " + str(max_deposit))
+
